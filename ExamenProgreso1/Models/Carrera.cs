e6f341cb0d082ff07518f3c93ebe935c0f581cc6
Justxt <jostinmatt@gmail.com>
2024-04-22 07:36:41 -0500

    Add project files.

---
+++ ExamenProgreso1/Models/Carrera.cs
@@ -0,0 +1,11 @@
+﻿namespace ExamenProgreso1.Models
+{
+    public class Carrera
+    {
+        public string Id { get; set; }
+        public string? NombreCarrera { get; set; }
+        public string? Campus {  get; set; }
+        public int? NumeroSemestres { get; set; }
+
+    }
+}
---
+++ ExamenProgreso1/Models/Estudiante.cs
@@ -0,0 +1,15 @@
+﻿namespace ExamenProgreso1.Models
+{
+    public class Estudiante
+    {
+        public int Id { get; set; }
+        public string? Nombre {  get; set; }
+        public int NumeroTelefono { get; set; }
+        public decimal Sueldo { get; set; }
+        public Boolean EsMayorDeEdad {  get; set; }
+        public DateTime? FechaNacimiento { get; set; }
+
+
+        public Carrera carrera { get; set; }
+    }
+}
