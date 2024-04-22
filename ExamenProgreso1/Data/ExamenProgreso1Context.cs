6d96d9b7284eb5dd5b1016ca74096bdced5a1142
Justxt <jostinmatt@gmail.com>
2024-04-22 07:45:15 -0500

    Controlador Entity lsita, bd lista para desplegar

---
+++ ExamenProgreso1/Data/ExamenProgreso1Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ExamenProgreso1.Models;
+
+namespace ExamenProgreso1.Data
+{
+    public class ExamenProgreso1Context : DbContext
+    {
+        public ExamenProgreso1Context (DbContextOptions<ExamenProgreso1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ExamenProgreso1.Models.Estudiante> Estudiante { get; set; } = default!;
+    }
+}
