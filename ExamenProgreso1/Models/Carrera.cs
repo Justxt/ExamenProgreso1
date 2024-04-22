f99fa2af5935851f6917b4dffd4ab2d4c2edee3e
Justxt <jostinmatt@gmail.com>
2024-04-22 07:40:50 -0500

    Validaciones agregadas

--- ExamenProgreso1/Models/Carrera.cs
+++ ExamenProgreso1/Models/Carrera.cs
@@ -1,10 +1,15 @@
-﻿namespace ExamenProgreso1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExamenProgreso1.Models
 {
     public class Carrera
     {
         public string Id { get; set; }
+        [Required]
         public string? NombreCarrera { get; set; }
+        [Required]
         public string? Campus {  get; set; }
+        [Required]
         public int? NumeroSemestres { get; set; }
 
     }
--- ExamenProgreso1/Models/Estudiante.cs
+++ ExamenProgreso1/Models/Estudiante.cs
@@ -1,12 +1,16 @@
-﻿namespace ExamenProgreso1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExamenProgreso1.Models
 {
     public class Estudiante
     {
         public int Id { get; set; }
+        [Required]
         public string? Nombre {  get; set; }
         public int NumeroTelefono { get; set; }
         public decimal Sueldo { get; set; }
         public Boolean EsMayorDeEdad {  get; set; }
+        [Required]
         public DateTime? FechaNacimiento { get; set; }
 
 
